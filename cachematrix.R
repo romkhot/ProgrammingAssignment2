9f7db2efa3d0c0f3934a48f392276b28b6285428
romkhot <xrom@zeos.net>
2015-02-21 20:04:58 +0200

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions could be used to cache the inverse of a matrix with some benefits in performance.
 
-## Write a short comment describing this function
-
+##1. The makeCacheMatrix function returns a list of functions to perform:  ##
+#    set/get the value of matrix
+#    set/set the value of inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+   inv <- NULL
+   set_ <- function(y) {
+      x <<- y
+      inv <<- NULL
+   }
+   get_ <- function() x
+   set_inverse <- function(inverse) inv <<- inverse
+   get_inverse <- function() inv
+   list(set=set_, get=get_, setInverse=set_inverse, getInverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
-
+##2. The cacheSolve function returns the inverted matrix.  ##
+#    If the inverse has already been computed, it returns the cached result without re-computation. 
+#    Otherwise it computes the inverse, then sets the value in the cache using the setInverse function.
+#    Assuming that a matrix is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        #
+   inv_matr <- x$getInverse()
+   if(!is.null(inv_matr)) {
+      message("Cached data!")
+   } else {     
+     inv_matr <- solve(x$get())
+     x$setInverse(inv_matr)
+   }
+   inv_matr
 }
+
+##3. Example of usage: ##
+m <- matrix(1, nrow = 3, ncol = 3)
+m[1,1] = 10; 
+m[2,2] = 10;
+m[3,3] = 10;
+cm <- makeCacheMatrix(m)
+cm$get()
+#      [,1] [,2] [,3]
+# [1,]   10    1    1
+# [2,]    1   10    1
+# [3,]    1    1   10
+
+## First run - calculation:
+cacheSolve(cm)
+#              [,1]         [,2]         [,3]
+# [1,]  0.101851852 -0.009259259 -0.009259259
+# [2,] -0.009259259  0.101851852 -0.009259259
+# [3,] -0.009259259 -0.009259259  0.101851852
+
+## Second run - reading from the cache:
+cacheSolve(cm)
+# Cached data!
+#              [,1]         [,2]         [,3]
+# [1,]  0.101851852 -0.009259259 -0.009259259
+# [2,] -0.009259259  0.101851852 -0.009259259
+# [3,] -0.009259259 -0.009259259  0.101851852
